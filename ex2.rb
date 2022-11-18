502a6555010b17882d0c8dc3d5b523f94a898cc9
JonathanJoseGonzalez <jonathanjosegonzalez1996@hotmail.com>
2022-11-18 08:27:54 -0300

    Testando variáveis

---
+++ ex2.rb
@@ -0,0 +1,24 @@
+#Variables = I was learning Variables
+
+age = 18
+puts age
+age = age + 5
+puts age
+age +=5
+puts age
+
+age = 20
+age -=14
+puts age
+
+cash = 10
+cash *=2
+puts cash
+
+temperature = 40
+temperature /=10
+puts temperature
+
+puts "Hi! What is your name".upcase
+name = gets.chomp
+puts "#{name}, you're great! ".upcase
