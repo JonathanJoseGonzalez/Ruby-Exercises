625f29a020aca84bba83fac08299c8572b0efcae
JonathanJoseGonzalez <jonathanjosegonzalez1996@hotmail.com>
2022-11-24 07:11:37 -0300

    ex3.rb
    Testando os métodos de input e output. Ainda estou meio confuso sobre como deixar o texto alinhadinho. E ainda não sei usar a função "nil".

---
+++ ex3.rb
@@ -0,0 +1,11 @@
+puts "Testing the inputs and outputs"
+puts "Testing nº 2"
+puts "Testing the \"print\" output"
+
+puts " \n What is your name?"
+name = gets.chomp
+puts "Hello #{name}, my name is John. I'm testing and learning the Ruby programming language."
+puts "How old are you?"
+age = gets
+puts "#{age}? Wow. You look older kkkk no offense, of course kkk"
+print "Anyway, there is not much to say or do. This is/was the exercise. 'til the next time!"
