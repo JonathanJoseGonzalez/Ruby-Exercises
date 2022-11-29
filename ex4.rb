aa71d754bbda5d7318650db7c990518478451909
JonathanJoseGonzalez <jonathanjosegonzalez1996@hotmail.com>
2022-11-29 07:48:25 -0300

    Fiz uma pequena alteração
    Fui testar as condições e não consegui. Ainda não entendo como elas funcionam no Ruby. Deixei tudo com um # na frente, como anotação. Volto (ou não) quando entender melhor.

--- ex4.rb
+++ ex4.rb
@@ -1,26 +1,20 @@
-5 == 5
-5 == 6
-puts "\n"
-5!=5
-5!=6
-puts "\n"
-5>4
-5>6
-puts "\n"
-5<6
-5<4
-puts "\n"
-5>=5
-5>=9
-puts "\n"
-5.eql?(5.0)
-5.eql?(5)
-puts "\n"
-a = 6
-b = 6
-a.equal?(b)
-puts "\n"
-5 <=> 10
-5 <=> 5
-10 <=> 5
-puts "\n"
+#puts "Let's play a litte game. (I'm just testing the boolean stuff)"
+#puts "What color do you like the most?"
+#color = gets
+#if color = blue
+#  puts "You like, blue, eh? Nice. Reminds of Yoru, from Valorant"
+#elsif color = yellow
+#  puts "Personally, I'm not a big fan of yellow. It is, though, the dominant color of my national flag. Brazil is doing great at the World Cup!"
+#elsif color = red
+#  puts "I like red, too. But not all tones of red. Just the metallic and dark ones - like wine, get it?"
+#elsif color = green
+#  puts "Personally, I'm not a big fan of green. IDK, I just don't like or dislike it."
+#elsif color = orange
+#  puts "Eminem knows what rhymes with orange. I bet you don't kkkk"
+#elsif color = purple
+#  puts "Now, we're talking. 🗿🍷 (if you can't see the emojis, it is the moai and the wine ones)"
+#elsif color = gray
+#  puts "Classy, eh? Suits you..."
+#elsif color = golden
+#  puts "Fancy bitch, kkkk can't blame you, though..."
+#end
