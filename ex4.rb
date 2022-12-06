339327ff7eb0239f1682296b27a3f9a7d29a639d
JonathanJoseGonzalez <jonathanjosegonzalez1996@hotmail.com>
2022-12-06 14:09:17 -0300

    Exercício 4
    Fiz tanta alteração e essa joça nao rodou no Terminal. Vou colocar ele assim mesmo e passar pro próximo.

--- ex4.rb
+++ ex4.rb
@@ -1,20 +1,22 @@
-#puts "Let's play a litte game. (I'm just testing the boolean stuff)"
-#puts "What color do you like the most?"
-#color = gets
-#if color = blue
-#  puts "You like, blue, eh? Nice. Reminds of Yoru, from Valorant"
-#elsif color = yellow
-#  puts "Personally, I'm not a big fan of yellow. It is, though, the dominant color of my national flag. Brazil is doing great at the World Cup!"
-#elsif color = red
-#  puts "I like red, too. But not all tones of red. Just the metallic and dark ones - like wine, get it?"
-#elsif color = green
-#  puts "Personally, I'm not a big fan of green. IDK, I just don't like or dislike it."
-#elsif color = orange
-#  puts "Eminem knows what rhymes with orange. I bet you don't kkkk"
-#elsif color = purple
-#  puts "Now, we're talking. 🗿🍷 (if you can't see the emojis, it is the moai and the wine ones)"
-#elsif color = gray
-#  puts "Classy, eh? Suits you..."
-#elsif color = golden
-#  puts "Fancy bitch, kkkk can't blame you, though..."
-#end
+puts "Let's play a litte game. (I'm just testing the boolean stuff)"
+puts "What color do you like the most?"
+color = gets.chomp
+if color == "blue"
+  puts "You like, blue, eh? Nice. Reminds of Yoru, from Valorant"
+elsif color == "yellow"
+  puts "Personally, I'm not a big fan of yellow. It is, though, the dominant color of my national flag. Brazil is doing great at the World Cup!"
+elsif color == "red"
+  puts "I like red, too. But not all tones of red. Just the metallic and dark ones - like wine, get it?"
+elsif color == "green"
+  puts "Personally, I'm not a big fan of green. IDK, I just don't like or dislike it."
+elsif color == "orange"
+  puts "Eminem knows what rhymes with orange. I bet you don't kkkk"
+elsif color == "purple"
+  puts "Now, we're talking. 🗿🍷 (if you can't see the emojis, it is the moai and the wine ones)"
+elsif color == "gray"
+  puts "Classy, eh? Suits you..."
+elsif color == "golden"
+  puts "Fancy bitch, kkkk can't blame you, though..."
+else
+  puts "color is blank"
+end
