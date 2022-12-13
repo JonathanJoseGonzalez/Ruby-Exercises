e1a73f9e9030fe934fe47acc7b47c519d39a6085
JonathanJoseGonzalez <jonathanjosegonzalez1996@hotmail.com>
2022-12-13 10:15:27 -0300

    Update ex6.rb

--- ex6.rb
+++ ex6.rb
@@ -19,3 +19,19 @@ puts "Again, I'm just testing the loops. Will you go to the prom with me?"
 until gets.chomp == "yes" do
     puts "Will you go to the prom with me?"
 end
+
+for i in 0..5                       # Esta parte é parecida - se não igual kk - ao "para" ensinado na lógica de programação. Consiste na fórmula:
+    puts "#{i} zombies are coming!" # 'for' = para, 'in' = até [coloca-se aqui o número de vezes], o que quer que seja feito
+end                                 #e este 'end'
+
+5.times do
+    puts "Zombies are coming. They killed the entire army in front of the hospital!"
+end
+
+5.times do |number|
+    puts "Alternative fact number #{number}"
+end
+
+5.upto(10){|num|print"#{num}"}
+
+10.downto(5){|num|print"#{num}"}
